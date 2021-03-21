4ad9a4e2eefe1b7a5d9dc47aed63c84ced951d7d
smartpath10 <smartpath10@3ss.tv>
2021-03-21 14:02:07 +0200

    adding blank tests

---
+++ src/test/java/TestApp.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+public class TestApp {
+
+    @Test
+    public void test1(){
+
+    }
+
+    @Test
+    public void test2(){
+
+    }
+
+    @Test
+    public void test3(){
+
+    }
+}
