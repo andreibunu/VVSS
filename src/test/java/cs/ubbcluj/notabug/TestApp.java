8860332e8c89c391dd275d1d8eb776c1dbb6c40f
smartpath10 <smartpath10@3ss.tv>
2021-03-22 13:24:43 +0200

    added one test

--- src/test/java/cs/ubbcluj/notabug/TestApp.java
+++ src/test/java/cs/ubbcluj/notabug/TestApp.java
@@ -1,5 +1,6 @@
 package cs.ubbcluj.notabug;
 
+import app.domain.Student;
 import app.service.Service;
 import org.junit.After;
 import org.junit.Before;
@@ -16,7 +17,13 @@ public class TestApp {
 
     @Test
     public void test1(){
-
+        Student student = new Student(TestBuilder.ID, TestBuilder.name, TestBuilder.group, TestBuilder.email);
+        assert (testService.findStudent(student.getID()) != student);
+        testService.addStudent(student);
+        assert (testService.findStudent(student.getID()).getID().equals(student.getID()));
+        assert (testService.findStudent(student.getID()).getNume().equals(student.getNume()));
+        assert (testService.findStudent(student.getID()).getEmail().equals(student.getEmail()));
+        assert (testService.findStudent(student.getID()).getGrupa() == student.getGrupa());
     }
 
     @Test
@@ -26,7 +33,9 @@ public class TestApp {
 
     @After
     public void after(){
-
+        testService.deleteStudent(TestBuilder.ID);
     }
-    
+
+
+
 }
